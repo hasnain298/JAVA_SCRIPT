bbfc9a2be31384a709e8d556a7d19325e237c7e1
hasnain298 <hasnainkhanjadoon550@gmail.com>
2025-11-01 11:38:25 +0500

    Main

---
+++ app.js
@@ -0,0 +1,215 @@
+// console.log("JS IS RUNNING!--->" );
+
+
+
+// QNO1 STARTS HERE
+
+// let studentsInFuture = [];
+
+// studentsInFuture.push("HASNAIN");
+
+// studentsInFuture.push("UMAIR");
+
+// console.log(studentsInFuture);
+
+// QNO1 ENDS HERE 
+
+
+// QNO2 STARTS HERE 
+
+// let object = [];
+// object.push("Hasnain");
+// object.push("Umair");
+// console.log(object)
+
+// QNO2 ENDS HERE 
+
+
+// QNO3 STARTS HERE 
+
+// let ourFriends = [ ];
+// ourFriends = ["Hasnain" , "Umair" , "Atif" , "Anas"];
+// console.log(ourFriends);
+
+// QNO3 ENDS HERE 
+
+
+// QNO4 STARTS HERE 
+
+// let numbers = [];
+// numbers = [1,2,3,4,5,6,7,8,9,0];
+// console.log(numbers)
+
+// QNO4 ENDS HERE 
+
+// QNO5 STARTS HERE 
+
+// let boolean = [];
+// boolean = [true , false  ];
+// console.log(boolean);
+
+// QNO5 ENDS HERE 
+
+
+// QNO6 STARTS HERE 
+
+// let mixArr = [];
+// mixArr = [true , "Harry" , 38781, false , undefined , "Js" , 1234, -3212];
+// console.log(mixArr);
+
+// QNO6 ENDS HERE 
+
+
+// QNO7 STARTS HERE 
+
+// let pak = [];
+// pak = ["B.COM", "SSC" , "HSC" , "MSC" , "PHD" , "MS"  , "BSC", "BS", "Phil"];
+// console.log(pak);
+
+// QNO7 ENDS HERE 
+
+
+
+// QNO8 STARTS HERE 
+
+// let students = ["Anas" , "Hasnain" , "Umair"];
+// let marks = [439 , 432 , 323];
+// console.log( "NAME OF STUDENTS" ,  students,);
+// console.log("TOTAL MARKS ===> 500!");
+// console.log("MARKS OF STUDENTS" , marks);
+
+
+
+// QNO8 ENDS HERE 
+
+
+
+
+// QNO9 STARTS HERE 
+
+// part "a"
+
+// let colors = [];
+// let userColor = prompt("Enter Your Color That you want in the beggining i also have three colors red green and blue ok!")
+// colors = ["red" , "green" , "blue" ];
+// console.log(colors.unshift(userColor));
+// console.log(colors);
+
+
+// PART "b"
+
+// let colors = [];
+// colors = ["Red" , "Green" , "Blue"];
+// let userColor = prompt("Enter Color That You Want To Add in The End!")
+// console.log(colors.push(userColor));
+// console.log(colors);
+
+
+// PART "c"
+
+// let colors = ["Green" , "Blue" , "Red"];
+// let usercolor = prompt("Enter Two colors That you want to Add in the beggining!separated with commas");
+// console.log(colors.unshift(usercolor));
+// console.log(colors);
+
+// PART "d"
+
+
+// let colors = ["Red" , "Blue" , "Green"];
+// console.log(colors)
+// console.log("Removed The First Color From Shift---->" , colors.shift());
+// console.log("New Output--->" ,colors);
+// console.log("Removed The Last Color From Pop----> " ,colors.pop());
+// console.log(" New Output---->" ,colors);
+
+
+
+// PART "e//"
+
+// let numbers = [1,2,3,4];
+// let userNumber = prompt("Enter The Index Number Where You Want To Add A Number!");
+// let number = prompt(" Now , Enter the Number You Want!")
+// numbers.splice(userNumber,0 ,number);
+// console.log(numbers);
+
+
+// PART "f"
+
+// let colors = ["Red" , "Green" , "Blue" ];
+// // console.log("Before Delete---->" ,colors);
+// let index = prompt("At Which Index You Want to Delete Color");
+// let del = prompt("How Many Color You Delete!");
+// console.log(colors.splice(index, del));
+// console.log("After Delete---->",colors);
+
+
+
+// QNO10 Starts Here //
+
+// let arr = [88 , 33 ,93 , 37 ,67 , 65 , 0 , 11 ,99.99];
+// console.log("Scores Of Students" , arr);
+// console.log("Orderd Score Of Students" ,arr.sort());
+
+
+// QNO10 Ends Here elet 
+
+
+// Qno11 Starts Here 
+
+
+// let cities = ["Karachi" , "Lahore" , "Islamabad" , "Quetta" , "Peshawar"];
+// console.log("Cities---->" ,cities);
+// console.log("Selected Cities--->" , cities.slice(0,3));
+
+
+// Qno11 Ends Here 
+
+
+// Qno12 Starts Here 
+
+// var arr = ["I" , "am" , "Hasnain"];
+// console.log(arr);
+// var arr = ["I am Hasnain"];
+// console.log(arr);
+
+// Qno12 Ends Here 
+
+// let arr = [];
+// // console.group( "Pop" , arr.pop());
+// // console.log(arr);
+// // console.log( "Shift", arr.shift());
+// // console.log(arr);
+// // console.log("Push" ,arr.push(10 , "Hasnain"));
+// // console.log(arr);
+// // console.log(arr.unshift(1));
+// // console.log(arr);
+
+// // console.log(arr.splice(0,11));
+// // console.log(arr);
+
+
+// console.log(arr.sort());
+
+
+// let value = [];
+
+// value.push('Banana');
+// console.log(value);
+
+// value.push("Apple");
+// console.log(value);
+
+// value.push('Cherry');
+// console.log(value);
+
+
+
+// value.shift();
+// console.log(value);
+
+// value.shift();
+// console.log(value);
+
+
+// let arr = ["O"]
+
