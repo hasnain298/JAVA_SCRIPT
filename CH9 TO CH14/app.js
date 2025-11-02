19c312ed098983f6771f4070a948fa24ab0f7507
hasnain298 <hasnaincoder15@gmail.com>
2025-11-02 23:52:19 +0500

    JS

--- CH9 TO CH14/app.js
+++ CH9 TO CH14/app.js
@@ -204,6 +204,12 @@
 // if (userAnswer == 7){
 //     alert("Bingo! Correct Answer");
 // }
+// else if (userAnswer > 1 && userAnswer < 6 ){
+//     alert("Very Close ,Try Again!")
+// }
+// else if(userAnswer > 7 && userAnswer <= 10){
+//     alert("Just Come Downwards You Get It!")
+// }
 // else{
 //     alert("Close Enough to secret Number");
 // }
