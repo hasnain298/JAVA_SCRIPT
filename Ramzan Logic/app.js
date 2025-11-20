c39cc0bfde76f266f8a4cd1edec06c1e857d2e07
hasnain298 <hasnainkhanjadoon550@gmail.com>
2025-11-20 13:45:57 +0500

    Ramzan Logic Done

---
+++ Ramzan Logic/app.js
@@ -0,0 +1,45 @@
+// console.log("Js Is Running!");
+
+// function findRamzanTime (){
+// let today = new Date();
+// // console.log(today)
+
+// let ramzanDate = new Date("18 Feb 2026");
+
+
+// let diff =   ramzanDate.getTime() - today.getTime() ;
+// // console.log(diff)
+
+
+// console.log("====================================")
+// console.log(`Following Duration Left In Ramzan 2026`)
+
+// let totalMonts = diff / (1000 * 60 * 60 *24 * 30);
+// console.log("Total Months ---->" , Math.round(totalMonts));
+
+// let totalDays = diff / (1000* 60 * 60 * 24)
+// console.log("Total Days ----> " , Math.round(totalDays));
+
+
+// let totalWeeks = diff / (1000 * 60 * 60 * 24 * 7);
+// console.log("Total Weeks ---->" ,Math.round(totalWeeks));
+
+// let totalHours = diff / (1000 * 60 * 60);
+// console.log( "Total Hours" , Math.round(totalHours))
+
+
+// let totalMinutes = diff / (1000 * 60);
+// console.log( "Total Minutes" , Math.round(totalMinutes));
+
+
+// let totalSeconds = diff / (1000);
+// console.log("Total Second ---->" , Math.round(totalSeconds));
+
+// console.log("Total Milliseconds ---->" , diff)
+
+
+
+// }
+
+
+// findRamzanTime()
