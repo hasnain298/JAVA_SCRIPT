e4c0348dd900c81e01dec43c7e787f85e87b72bc
hasnain298 <hasnainkhanjadoon550@gmail.com>
2025-11-24 20:35:31 +0500

    Js Assignment CH38 to CH40

---
+++ CH38 TO CH40/app.js
@@ -0,0 +1,146 @@
+console.log("Js Is Running");
+
+
+
+// Problem No 1 
+// function power(a,b){
+//   let result = 1;
+//   for(let i = 0; i < b; i++){
+//     result *= b;
+//     // console.log(result)
+//   }
+//   return result
+// }
+
+
+
+// console.log(power(12,6))
+
+
+
+
+
+// Problem No2
+
+// function leapYear(){
+// let user = +prompt("Enter Year From 2000s");
+// switch(user){
+//     case 2000 : console.log(" Leap Year")
+// break;
+//     case 2004 : console.log("Leap Year")
+// break;  
+//     case 2008 : console.log("Leap Year")
+//  break;   
+//     case 2012 : console.log("Leap Year")
+// break;    
+//     case 2016 : console.log("Leap Year")
+// break;
+//     case 2020 : console.log("Leap Year")
+// break;
+//     case 2024 : console.log("Leap Year")
+//     break;
+//     default : console.log("Invalid Year")
+// }
+
+
+// }
+// leapYear()
+
+
+
+// Problem NO 3
+
+// function calcSemiPerimeter(a,b,c){
+// let S = (a+b+c)/2
+// return S
+// }
+
+// console.log(calcSemiPerimeter(3,8,11))
+
+
+// function calcArea(a,b,c){
+//     let S = 11;
+//     let area = Math.sqrt(S *(S-a) * (S-b) * (S-c));
+//     return area
+// }
+// console.log(calcArea(2,4,1))
+
+
+
+
+// Problem No 4
+// console.log("==================================")
+
+// function marksRecieved(sub1,sub2,sub3){
+// console.log("Subject One ",sub1,"Marks")
+// console.log("Subject Two",sub2 , "Marks")
+// console.log("Subject Three",sub3, "Marks")
+// }
+
+// marksRecieved(59,73,87)
+
+// function averageMarks(){
+// let sub1  = 59;
+// let sub2 = 73;
+// let sub3 = 87;
+
+// let totalMarks = 300;
+// console.log("Total Marks ----> " , totalMarks)
+// let averageMarks = (sub1+sub2+sub3);
+// console.log("Average Marks ----> ",averageMarks);
+// }
+// averageMarks()
+
+
+// function calcPercentage(totalMarks , averageMarks){
+
+// let Percentage =  (averageMarks / totalMarks ) * 100
+
+
+// return Percentage
+// }
+
+// console.log("Percentage ---->" , calcPercentage(300 , 219),"%");
+
+
+
+
+// function checkEligibilityWord(){
+//     let check = 0;
+// let para = "The Quick Brown Fox".toLowerCase();
+// let  letter  = "o";
+// for(let i = 0; i <= para.length; i++){
+//     if(para[i] === letter){
+//     check++;
+//     }
+// }return check
+
+
+// }
+
+// console.log(checkEligibilityWord());
+
+
+// Problem No 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
