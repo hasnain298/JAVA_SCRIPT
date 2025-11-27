d31d50af13fbe216074df50d767193958d0e8a76
hasnain298 <hasnainkhanjadoon550@gmail.com>
2025-11-27 21:28:24 +0500

    Get User Input

---
+++ Facebook Get Input Using JS/app.js
@@ -0,0 +1,10 @@
+console.log("Js Is Running!");
+
+function check(){
+let emailInput = document.getElementById("email");
+let passInput = document.getElementById("num");
+
+
+console.log(emailInput.value);
+console.log(passInput.value);
+}
---
+++ Facebook Get Input Using JS/app2.js
@@ -0,0 +1,15 @@
+console.log("Js Is Running!");
+
+
+
+function func(){
+    let firstName = document.getElementById("fn");
+    let lastName = document.getElementById("ln");
+    let mobile = document.getElementById("mb")
+    let password = document.getElementById("np");
+
+    console.log("First Name --->",firstName.value);
+    console.log("Last Name --->",lastName.value);
+    console.log("Mobile or Email --->",mobile.value);
+    console.log("Password --->",password.value);
+}
