fb51b966e1f95073ada90dc4449a0a1d48a7ea67
hasnain298 <hasnainkhanjadoon550@gmail.com>
2025-11-29 11:26:45 +0500

    Js Assignment CH41 TO CH44

---
+++ CH41 TO CH44/app.js
@@ -0,0 +1,57 @@
+console.log("Js Is Running!");
+
+// Problem No 1
+
+// var str = "my name is hasnain".split("");
+// var vowel = "aeiou".split("");
+// var count = 0;
+
+// function countAllVowels() {
+//     for (var i = 0; i < str.length; i++) {
+//         var currentChar = str[i].toLowerCase();
+
+//         for (var j = 0; j < vowel.length; j++) {
+//             if (currentChar === vowel[j]) {
+//                 count++; 
+//                 break; 
+//             }
+//         }
+//     }
+    
+//     console.log("Total number of vowels found: " + count); 
+// }
+
+// countAllVowels(); 
+
+
+// Problem No 2
+// function removeVowels(){
+// let str = "my name is hasnain";
+// console.log(str)
+// let newStr = str.replace(/[aeiou]/g , '');
+// return newStr
+
+
+
+// }
+
+// console.log(removeVowels());
+
+
+// Pronblem No 3
+// function changeQuantities(){
+// let user = document.getElementById("inp");
+// let changeInKm = user.value * 1000
+// console.log("Value In Meters" , Math.round(changeInKm) , "meters");
+// let changeInFeet = user.value * 3280.84;
+// console.log("Value In Feet" , Math.round(changeInFeet) , "feet");
+// let changeInInches = user.value * 39370.1;
+// console.log("Value In Inches" , Math.round(changeInInches) , "inches");
+// let changesInCentimeters = user.value * 100000;
+// console.log("Value In Centimeter",Math.round(changesInCentimeters) , "centimeter");
+
+
+
+
+// }
+
