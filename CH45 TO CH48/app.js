955e63abd02f537ebc7426db7c3d74af6efdd9b7
hasnain298 <hasnainkhanjadoon550@gmail.com>
2025-12-07 17:33:14 +0500

    Js Assignment One Question From Sir Hasan

---
+++ CH45 TO CH48/app.js
@@ -0,0 +1,164 @@
+console.log("Js Is Running!");
+
+
+
+// PROBLEM NO 1
+// function myAlert(){
+//     alert("Hello, World!")
+// }
+
+// // PROBLEM NO 2
+// function one(){
+//     alert("Thank You For Purchasing Mobile")
+// }
+// function three(){
+//     alert("Thank You For Purchasing Mobile")
+// }
+// function two(){
+//     alert("Thank You For Purchasing Mobile")
+// }
+// function four(){
+//     alert("Thank You For Purchasing Mobile")
+// }
+
+
+//  PROBLEM NO 3
+
+// function one(){
+//     document.getElementById("one").innerText = ""
+// }
+// function two(){
+//     document.getElementById("two").innerText = ""
+
+// }
+// function three(){
+//     document.getElementById("three").innerText = ""
+
+// }
+// function four(){
+//     document.getElementById("four").innerText = ""
+
+// }
+
+
+// function five(){
+//     document.getElementById("five").innerText = ""
+
+// }
+
+
+// // PROBLEM NO 4
+
+
+// document.getElementById("para");
+// document.getElementById("btn");
+
+
+
+
+// function one(e){
+//     para.innerText = "2000 RS";
+//     btn.innerText = "Decrease"
+// }
+
+
+// function two(e){
+//     para.innerText = "1000RS";
+//     btn.innerText = "Increase"
+// }
+
+// function func1(){
+//     if(btn.innerText == "Increase"){
+//         return one(this)
+//     }else{
+//         return two(this)
+//     }
+// }
+
+
+
+
+
+// document.getElementById("para2");
+// document.getElementById("btn2");
+
+// function first(e){
+//     para2.innerText = "4000 RS";
+//     btn2.innerText = "Decrease"
+// }
+
+
+// function second(e){
+//     para2.innerText = "2000RS";
+//     btn2.innerText = "Increase"
+// }
+
+// function func2(){
+//     if(btn2.innerText == "Increase"){
+//         return first(this)
+//     }else{
+//         return second(this)
+//     }
+// }
+
+
+
+
+
+// document.getElementById("para3");
+// document.getElementById("btn3")
+
+
+
+// function one1(e){
+//     para3.innerText = "10000 RS";
+//     btn3.innerText = "Decrease"
+// }
+
+
+// function two2(e){
+//     para3.innerText = "5000RS";
+//     btn3.innerText = "Increase"
+// }
+
+// function func3(){
+//     if(btn3.innerText == "Increase"){
+//         return one1(this)
+//     }else{
+//         return two2(this)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Sir Hasan Question
+function morefn(e){
+    para.innerText = "My Name Is Hasnain. I am a Full Stack Developer From Pakistan";
+    btn.innerText = "See Less"
+}
+function lessfn(e){
+    para.innerText = "My Name Is Hasnain."
+    btn.innerText = "See More"
+
+}
+function expand(){
+if(btn.innerText == "See More"){
+    return morefn(this)
+}else{
+    return lessfn(this)
+}
+}
+
+
+
