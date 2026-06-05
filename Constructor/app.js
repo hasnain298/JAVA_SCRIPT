aa021c7a3ee3c2bb5b5f4d9903a9caa9ac58c894
hasnaincoder550 <hasnainkhanjadoon550@gmail.com>
2026-06-05 14:46:31 +0500

    Constructor Examples Assignment

---
+++ Constructor/app.js
@@ -0,0 +1,49 @@
+console.log("Js Is Running!");
+
+// function Students(name , fatherName , classe , rollno){
+//     this.name = name 
+//     this.fatherName = fatherName 
+//     this.classe = classe
+//     this.rollno = rollno 
+// }
+
+// let s1 = new Students("Muhammad Hasnain" , "Muhammad Naeem" , "XI" , "19")
+// let s2 = new Students("Muhammad Umair" , "Muhammad Arfeen" , "XIII" , "21")
+// console.log(s1 , s2)
+
+
+// Example # 2
+
+// Shoes Brands Constructor
+
+
+// function ShoesBrands(brand , price , warranty , discount){
+//     this.brand = brand 
+//     this.price = price
+//     this.warranty = warranty 
+//     this.discount = discount 
+// }
+
+// let s1 = new ShoesBrands("Nikee" , "887$" , "3 Years" , "30%");
+// let s2 = new ShoesBrands("Reebook" , "1087$" , "2.5 Years" , "10%");
+// let s3 = new ShoesBrands("CR7" , "55087$" , "7 Years" , "5%");
+// let s4 = new ShoesBrands("Bata" , "55$" , "2 Years" , "52%");
+// console.log(s1,s2,s3,s4)
+
+
+// Workers Constructor
+
+// function Workers(job , experience , name , study){
+//     this.job = job 
+//     this.experience = experience 
+//     this.name = name 
+//     this.study = study 
+// }
+
+// let worker1 = new Workers("Web Development" , "3 Years" , "Muhammad Hasnain" , "Msc AI")
+// let worker2 = new Workers("Ethical Hacker" , "7 Years" , "Muhammad Umair" , "Graduate CIT")
+// let worker3 = new Workers("Cyber Security" , "4.5 Years" , "Muhammad Arafat" , "Diploma CS")
+// let worker4 = new Workers("Game Development" , "8 Years" , "Muhammad Ahmed" , "Bsc ")
+
+// console.log(worker1,worker2,worker3,worker4)
+
